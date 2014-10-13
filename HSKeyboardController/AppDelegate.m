ef46ae9a0dec1d7e45759b66a06e43ebbcc17699
horseshoe7 <ocon.sc@gmail.com>
2014-10-13 16:43:20 +0200

    added to barebones project

--- HSKeyboardController/AppDelegate.m
+++ HSKeyboardController/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "HSKeyboardController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [[HSKeyboardController sharedController] start];
+    
     return YES;
 }
 
